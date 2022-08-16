3a90502c6ec2d931cb9343b78e2585867dedb481
Eric Reeves <eric@alluvium.com>
2022-08-16 10:54:05 -0500

    Make hcp-packer public

--- modules/hcp_vault/main.tf
+++ modules/hcp_vault/main.tf
@@ -2,5 +2,5 @@ resource "hcp_vault_cluster" "vault" {
   hvn_id          = var.hvn_id
   cluster_id      = "${var.environment}-vault"
   tier            = var.hcp_vault_tier
-  public_endpoint = false
+  public_endpoint = true
 }
