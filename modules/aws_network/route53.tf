c505e1bb6efbf5ff39371173a3b55616cd50d3a6
Eric Reeves <eric@alluvium.com>
2022-08-20 21:54:09 -0500

    Stupid DNS

--- modules/aws_network/route53.tf
+++ modules/aws_network/route53.tf
@@ -10,7 +10,7 @@ resource "aws_route53_record" "cube" {
 resource "aws_route53_record" "hypercube" {
   allow_overwrite = true
   zone_id         = var.route53_hosted_zone_id
-  name            = "cube.${var.route53_domain_name}"
+  name            = "hypercube.${var.route53_domain_name}"
   type            = "A"
   ttl             = "60"
   records         = ["192.168.1.5"]
@@ -19,7 +19,7 @@ resource "aws_route53_record" "hypercube" {
 resource "aws_route53_record" "horizon" {
   allow_overwrite = true
   zone_id         = var.route53_hosted_zone_id
-  name            = "cube.${var.route53_domain_name}"
+  name            = "horizon.${var.route53_domain_name}"
   type            = "A"
   ttl             = "60"
   records         = ["192.168.1.6"]
