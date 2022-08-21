b5621fff89a0e2aa161f6eecd1a3e9307855a2ed
Eric Reeves <eric@alluvium.com>
2022-08-20 21:31:13 -0500

    Add DNS Records for home systems

---
+++ modules/aws_network/route53.tf
@@ -0,0 +1,26 @@
+resource "aws_route53_record" "cube" {
+  allow_overwrite = true
+  zone_id         = var.route53_hosted_zone_id
+  name            = "cube.${var.route53_domain_name}"
+  type            = "A"
+  ttl             = "60"
+  records         = "192.168.1.4"
+}
+
+resource "aws_route53_record" "hypercube" {
+  allow_overwrite = true
+  zone_id         = var.route53_hosted_zone_id
+  name            = "cube.${var.route53_domain_name}"
+  type            = "A"
+  ttl             = "60"
+  records         = "192.168.1.5"
+}
+
+resource "aws_route53_record" "horizon" {
+  allow_overwrite = true
+  zone_id         = var.route53_hosted_zone_id
+  name            = "cube.${var.route53_domain_name}"
+  type            = "A"
+  ttl             = "60"
+  records         = "192.168.1.6"
+}
