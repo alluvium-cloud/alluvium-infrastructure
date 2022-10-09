e4279d31c1464ba005734b8e871260104ea3e01e
Eric Reeves <eric@alluvium.com>
2022-10-09 11:48:53 -0500

    Drop Wireguard

--- main.tf
+++ main.tf
@@ -54,31 +54,31 @@ module "vault_kms" {
   pgp_key     = var.pgp_key
 }
 
-module "vpn_wireguard" {
-  source                 = "./modules/vpn_wireguard"
-  environment            = var.environment
-  region                 = var.region
-  vpc_id                 = module.aws_network.vpc_id
-  public_subnets_id      = module.aws_network.public_subnets_id
-  instance_type          = var.instance_type
-  ssh_public_key         = var.ssh_public_key
-  wg_server_private_key  = var.wg_server_private_key
-  wg_server_net          = var.wg_server_net
-  use_route53            = true
-  route53_domain_name    = var.route53_domain_name
-  route53_hosted_zone_id = var.route53_hosted_zone_id
-  home_cidr              = var.home_cidr
-  public_rtb_id          = module.aws_network.public_rtb_id
-  private_rtb_id         = module.aws_network.private_rtb_id
-  default_rtb_id         = module.aws_network.default_rtb_id
-  wg_clients = [
-    {
-      "friendly_name" = "${var.environment}-home"
-      "public_key"    = var.wg_client_public_key
-      "client_ip"     = var.wg_client_ip
-    }
-  ]
-}
+// module "vpn_wireguard" {
+//   source                 = "./modules/vpn_wireguard"
+//   environment            = var.environment
+//   region                 = var.region
+//   vpc_id                 = module.aws_network.vpc_id
+//   public_subnets_id      = module.aws_network.public_subnets_id
+//   instance_type          = var.instance_type
+//   ssh_public_key         = var.ssh_public_key
+//   wg_server_private_key  = var.wg_server_private_key
+//   wg_server_net          = var.wg_server_net
+//   use_route53            = true
+//   route53_domain_name    = var.route53_domain_name
+//   route53_hosted_zone_id = var.route53_hosted_zone_id
+//   home_cidr              = var.home_cidr
+//   public_rtb_id          = module.aws_network.public_rtb_id
+//   private_rtb_id         = module.aws_network.private_rtb_id
+//   default_rtb_id         = module.aws_network.default_rtb_id
+//   wg_clients = [
+//     {
+//       "friendly_name" = "${var.environment}-home"
+//       "public_key"    = var.wg_client_public_key
+//       "client_ip"     = var.wg_client_ip
+//     }
+//   ]
+// }
 
 // module "static_website" {
 //   source                 = "./modules/static_website"
