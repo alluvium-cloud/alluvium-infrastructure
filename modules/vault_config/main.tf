044cf78dc550d0c6a3288230f3fa3cad1fefdac0
Eric Reeves <eric@alluvium.com>
2022-11-27 22:25:09 -0600

    vault

--- modules/vault_config/main.tf
+++ modules/vault_config/main.tf
@@ -6,7 +6,7 @@ provider "vault" {
 #--------------------------------------
 # Create 'admin/alluvium' namespace
 #--------------------------------------
-resource "vault_namespace" "alluvium" {
-  provider = vault.admin
-  path = "alluvium"
-}
+# resource "vault_namespace" "alluvium" {
+#   provider = vault.admin
+#   path = "alluvium"
+# }
